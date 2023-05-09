c24f6a0e152ca72520750b1f8df4b9ff5a433465
Anil Nadiger <abnadiger@gmail.com>
2023-05-08 22:43:56 -0500

    port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function startApolloServer() {
     schema: buildSubgraphSchema({ typeDefs, resolvers }), 
   });
   
-  const port = process.env.port || 4003;
+  const port = process.env.PORT || 4003;
   const subgraphName = 'reviews';
 
   try {
